5a8b70d203bc2438331eda563ae4afb11e68d2a0
CwaR228 <sosok2gt@gmail.com>
2025-06-12 22:17:22 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const longRent = 7;
+  const shortRent = 3;
+  const basePrice = days * 40;
+  const basicDisc = basePrice - 20;
+  const additionalDisc = basePrice - 50;
+
+  if (days >= longRent) {
+    return additionalDisc;
+  }
+
+  if (days >= shortRent) {
+    return basicDisc;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
